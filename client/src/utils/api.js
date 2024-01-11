31fb385dd783aff2b78fb0cc7885d8bd8db3f58d
Shakwat18 <shakwathossen70@gmail.com>
2024-01-11 19:22:25 +0600

    buri change

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://real-state-kappa.vercel.app//api",
+  baseURL: "https://real-state-kappa.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
