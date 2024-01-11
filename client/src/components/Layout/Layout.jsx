f2e4b949683f3ada295f7fe8d35fd6ef2aa6c67e
Shakwat18 <shakwathossen70@gmail.com>
2024-01-11 19:13:01 +0600

    layout audience

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -27,7 +27,7 @@ const Layout = () => {
 
       const res = await getAccessTokenWithPopup({
         authorizationParams: {
-          audience: "http://localhost:8000",
+          audience: "https://real-state-kappa.vercel.app",
           scope: "openid profile email",
         },
       });
