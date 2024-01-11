db04a4d6c9a65aeb3c292ea293d258e50d8537f7
Shakwat18 <shakwathossen70@gmail.com>
2024-01-11 19:11:08 +0600

    jwt audienc url

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,7 +1,7 @@
 import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
-    audience: "http://localhost:8000",
+    audience: "https://real-state-kappa.vercel.app",
     issuerBaseURL: "https://dev-p73zinpt2ev606r7.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
